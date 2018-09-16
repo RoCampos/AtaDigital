0447279d71aeae9bc17920626883e8e9930e9a9e
Romerito Campos <romerito.campos@gmail.com>
2018-09-16 14:21:33 -0300

    modelos e relacionamentos adicionados

--- app/Projeto.php
+++ app/Projeto.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Projeto extends Model
 {
-    //
+    
+    /**
+     * Projeto has one Professor.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function professor()
+    {
+    	// hasOne(RelatedModel, foreignKeyOnRelatedModel = projeto_id, localKey = id)
+    	return $this->hasOne(Professor::class, 'id');
+    }
+
+    /**
+     * Projeto has many Atas.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function atas()
+    {
+    	// hasMany(RelatedModel, foreignKeyOnRelatedModel = projeto_id, localKey = id)
+    	return $this->hasMany(Ata::class);
+    }
+
+    /**
+     * Projeto has many Alunos.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function alunos()
+    {
+    	// hasMany(RelatedModel, foreignKeyOnRelatedModel = projeto_id, localKey = id)
+    	return $this->hasMany(Aluno::class);
+    }
 }
