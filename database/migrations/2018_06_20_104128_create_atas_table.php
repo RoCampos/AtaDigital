408f7e637910d8831b31dc99f2b9eea51f12dcfe
Romerito Campos <romerito.campos@gmail.com>
2018-09-16 01:04:39 -0300

    migrations necessárias a versão 0.1

--- database/migrations/2018_06_20_104128_create_atas_table.php
+++ database/migrations/2018_06_20_104128_create_atas_table.php
@@ -13,15 +13,22 @@ class CreateAtasTable extends Migration
      */
     public function up()
     {
+        //o coordenador pode ser substituído
+        //por isso manter id_prof e id_proj
         Schema::create('atas', function (Blueprint $table) {
             $table->increments('id');
             $table->string ('assunto');
             $table->string ('relatorio');
-            $table->unsignedInteger ('id_aluno');
+            $table->unsignedInteger ('id_prof');
+            $table->unsignedInteger ('id_proj');
             
-            $table->foreign ('id_aluno')
+            $table->foreign ('id_prof')
                 ->references ('id')
-                ->on('alunos');
+                ->on('professors');
+
+            $table->foreign ('id_proj')
+                ->references ('id')
+                ->on('projetos');
 
             $table->timestamps();
         });
